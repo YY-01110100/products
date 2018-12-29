4ce4683cb90c5305c6de9ef30eeaa272bf8029d7
Adam Zhang <q360465913@gmail.com>
2018-12-29 17:55:40 +0800

    add checking

--- products.py
+++ products.py
@@ -1,12 +1,16 @@
-#读取档案
-products = []
-with open('product.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,价格' in line
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+import os #operating system
+products = [] # 建立空清单
+if os.path.isfile('product.csv'):#检查档案是否在里面
+	print('yeah! 找到档案惹！')
+	with open('product.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,价格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到档案惹.....')
 
 # 让使用者输入
 while True:
@@ -23,7 +27,7 @@ for p in products:
 	print(p[0], '的价格是', p[1])
 
 # 写入档案
-with open('products.csv', 'w', encoding = 'utf-8') as f: #with 的功能是自动close
+with open('product.csv', 'w', encoding = 'utf-8') as f: #with 的功能是自动close
 	f.write('商品,价格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
